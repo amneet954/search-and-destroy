e657e461067345d09fed439570862b5503cfb6b1
Amneet Sandhu <amneet954@gmail.com>
2019-05-24 12:37:14 -0400

    MIN JUMP IN PROGRESS

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 // Complete this algo
 const minJumps = arr => {
+  console.log(arr);
 
+  let jumpLength = arr[0];
+  let maxDistance = 0;
+  let currentSpot = 0;
+  let nextPossibleJump = array.slice(currentSpot + 1, jumpLength);
+  console.log("dfjngds", nextPossibleJump);
 };
 
-module.exports = minJumps
+module.exports = minJumps;
